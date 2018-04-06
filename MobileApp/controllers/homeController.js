5a6292775a92749ada967374379916ad7262a6bc
Ganesh <ganeshxxyyzz@gmail.com>
2018-04-06 18:57:07 +0530

    6 March 2018

--- MobileApp/controllers/homeController.js
+++ MobileApp/controllers/homeController.js
@@ -1,16 +1,5 @@
 app.controller("homeCtrl",function($scope,$rootScope,$mdSidenav){
 
-// $scope.changeApple=function(){
-// $scope. $rootScope.json;
-// };
-//
-// $scope.toggleLeft=toggler("left");
-// function toggler (component){
-// return function(){
-//   $mdSidenav(component).toggle();
-// };
-// }
-
 $scope.toggleLeft = buildToggler('left');
    $scope.toggleRight = buildToggler('right');
 
@@ -19,5 +8,19 @@ $scope.toggleLeft = buildToggler('left');
        $mdSidenav(componentId).toggle();
      };
    }
+ })
 
-})
+// $scope.manufacturer=$rootScope.json.specs.manufacturer;
+// console.log(manufacturer);
+// $scope.manufacturer={};
+// for(var i=0;i<$rootScope.json.length;i++)
+// {
+//   $scope.manufacturer.push($rootScope.json.specs[i]);
+// }
+// console.log($scope.manufacturer);
+//
+// app.filter("filterManufacturer",function(){
+//   return function(){
+//
+//   }
+// })
